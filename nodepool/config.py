a9645a4665be0fc1f31cb993973240bf710eb2e1
Jianghua Wang <jianghua.wang@citrix.com>
2016-04-28 11:11:02 +0100

    Add namespace for nodepool
    If multiple nodepools share a single tenant, one nodepool's Servers
    may be deleted by other nodepools' cleanupLeakedInstances(). So add
    a nodepool name to identify each nodepool. Each nodepool should only
    clean Servers which are marked belong to it.

--- nodepool/config.py
+++ nodepool/config.py
@@ -1,3 +1,4 @@
+import os
 import os_client_config
 from six.moves import configparser as ConfigParser
 import yaml
@@ -95,6 +96,8 @@ def loadConfig(config_path):
     cloud_config = os_client_config.OpenStackConfig()
 
     newconfig = Config()
+    hostname = os.uname()[1]
+    newconfig.nodepool_name = config.get('nodepool-name', hostname)
     newconfig.db = None
     newconfig.dburi = None
     newconfig.providers = {}
@@ -137,6 +140,7 @@ def loadConfig(config_path):
 
     for provider in config.get('providers', []):
         p = Provider()
+        p.nodepool_name = newconfig.nodepool_name
         p.name = provider['name']
         newconfig.providers[p.name] = p
 
